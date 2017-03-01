feff8024de05ab275887a12ddb19f7c9d42d668a
Nicholas Leacock <nicholas@swissmail.com>
2017-03-01 20:19:33 +0000

    User Story 1
    As two Players,
    So we can play a personalised game of Battle,
    We want to Start a fight by entering our names and seeing them

--- app.rb
+++ app.rb
@@ -4,5 +4,15 @@ class Battle < Sinatra::Base
   get '/' do
     erb :index
   end
+#  run! if app_file == $0
+
+  post '/names' do
+    erb :play
+    puts params
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+  end
+
   run! if app_file == $0
+
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,7 +1,25 @@
+#require './app'
+# feature "Test Check" do
+#   scenario "Checking homepage text" do
+#     visit '/'
+#     expect(page).to have_content('Testing infrastructure working!')
+#   end
+# end
 
-feature "Test Check" do
-  scenario "Checking homepage text" do
+feature "Start a fight" do
+
+  scenario "take name of player 1" do
     visit '/'
-    expect(page).to have_content('Testing infrastructure working!')
+    fill_in('player_1', with: "Steve Rogers")
+    x = find_field('player_1').value
+    expect(x).to eq('Steve Rogers')
   end
-end
+
+    scenario "take name of player 2" do
+      visit '/'
+      fill_in('player_2', with: "Bruce Banner")
+      y = find_field('player_2').value
+      expect(y).to eq('Bruce Banner')
+    end
+
+  end
